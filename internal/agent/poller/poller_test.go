409ca7caf57cbc39ba335b667327eb78f90a85e4
Vasiliy Kozlyuk <kozluk.va@smtech.ru>
2024-09-28 23:46:29 +0300

    fix go vet

--- internal/agent/poller/poller_test.go
+++ internal/agent/poller/poller_test.go
@@ -1,6 +1,7 @@
 package poller
 
 import (
+	"github.com/Eqke/metric-collector/internal/agent/config"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap/zaptest"
 	"testing"
@@ -9,7 +10,7 @@ import (
 func TestNewPoller(t *testing.T) {
 	t.Run("poller_not_nil", func(t *testing.T) {
 		l := zaptest.NewLogger(t).Sugar()
-		poller := NewPoller(l)
+		poller := NewPoller(l, &config.AgentConfig{})
 
 		require.NotNil(t, poller)
 	})
